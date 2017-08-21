d88f9ce7563ed6843a80ab52c58d677e63fc1982
ddv12138 <644077730@qq.com>
2017-08-21 04:11:08 +0000

    Mon Aug 21 04:11:06 UTC 2017

--- init.sh
+++ init.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
 echo '开始自动部署'
-PWD=`pwd`
+pwd=`pwd`
+mysqlname="mysql-server"
+tomcatname="tomcat-server"
+phpname="php-server"
 echo "当前路径：${PWD}"
-echo "启动tomcat-server"
-docker run -d -p 80:8080 --name tomcat-server -v ${PWD}/tomcat-init/webroot/:/usr/local/tomcat/webapps/ROOT ddv12138/tomcat-with-mysql-driver
 echo "启动mysql-server"
-docker run -d -p 3306:3306 --name mysql-server -v ${PWD}/mysql-init/mysql-data/:/var/lib/mysql/ mysql:5.7
+docker run -d -p 3306:3306 --name ${mysqlname} -v ${pwd}/mysql-init/mysql-data/:/var/lib/mysql/ mysql:5.7
+echo "启动tomcat-server"
+docker run -d -p 80:8080 --name ${tomcatname} --link=${mysqlname}:mysql-server  -v ${pwd}/tomcat-init/webroot/:/usr/local/tomcat/webapps/ROOT ddv12138/tomcat-with-mysql-driver
 echo "启动php-server"
-docker run -d -p 8888:80 --name php-server -v ${PWD}/php-init/phpmyadmin/:/var/www/html/ ddv12138/php-phpmyadmin:latest 
+docker run -d -p 8888:80 --name ${phpname} -v ${pwd}/php-init/phpmyadmin/:/var/www/html/ ddv12138/php-phpmyadmin:latest 
